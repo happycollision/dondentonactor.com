14def4b1efcc7249f22b49c767b7d898299a0d32
Don Denton <don@happycollision.com>
2014-09-30 14:53:22 -0500

    Site nav complete
    Adds abbreviation for biography at certain sizes
    jQuery hides and shows at certain sizes
    Style updates

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
-
+$('.menu-icon').bind('click', function(){
+  if ($(this).hasClass('active')) {
+    $(this).removeClass('active');
+    $('.site-nav').addClass('js-hide');
+  } else {
+    $(this).addClass('active');
+    $('.site-nav').removeClass('js-hide');
+  }
+})
