a388490dd64f393e0667dba2b81eb5841fb9c1e9
Don Denton <don@happycollision.com>
2021-03-01 21:09:19 -0500

    Change bio to markdown

---
+++ src/components/Markdown.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" context="module">
+  import { markdown } from "$utils/markdown"
+  const defaultClasses = {
+    a: "link",
+  }
+</script>
+
+<script lang="ts">
+  export let source: string
+  export let embededClasses = defaultClasses
+  let className = ""
+  export { className as class }
+</script>
+
+<div class="via-markdown {className}">
+  {@html markdown(source, embededClasses)}
+</div>
