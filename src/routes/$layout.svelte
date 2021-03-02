96450c16f511388d9c87104a8e6f4544641c96c0
Don Denton <don@happycollision.com>
2021-03-01 21:54:28 -0500

    Fix footer position on short screens

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -6,14 +6,16 @@
   $: path = $page.path
 </script>
 
-<header class="font-sans" hidden="{path === '/'}">
-  <MainNav path="{path}" />
-</header>
+<div class="flex flex-col min-h-screen">
+  <header class="font-sans" hidden="{path === '/'}">
+    <MainNav path="{path}" />
+  </header>
 
-<div class="flex flex-col min-h-screen font-serif dark:bg-black">
-  <main class="m-4 flex-grow relative">
-    <slot />
-  </main>
+  <div class="flex-grow font-serif dark:bg-black">
+    <main class="m-4 relative">
+      <slot />
+    </main>
+  </div>
   <footer
     class="bg-green-500 text-white dark:bg-green-900 dark:text-gray-300 flex p-4 justify-between"
   >
