9c1f42d19b17036fa7bc34ef573c0da81eb195b8
Don Denton <don@happycollision.com>
2021-03-02 00:43:55 -0500

    Default all images to rounded corners

--- src/components/Img.svelte
+++ src/components/Img.svelte
@@ -2,6 +2,10 @@
   let srcIn: string
   export { srcIn as src }
   export let alt: undefined | string = undefined
+  /**
+   * set to true to prevent default corner rounding
+   */
+  export let noround = false
 
   function getSources(src: string): string[] {
     if (!src.startsWith("/img")) return [src]
@@ -25,4 +29,10 @@
   }
 </script>
 
-<img src="{src}" on:error="{nextSrc}" alt="{alt}" {...$$restProps} />
+<img
+  src="{src}"
+  on:error="{nextSrc}"
+  alt="{alt}"
+  class:rounded-lg="{!noround}"
+  {...$$restProps}
+/>
