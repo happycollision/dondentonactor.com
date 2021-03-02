a4cd5d8c9502fdfcfedb17c665d83b35fd9493dd
Don Denton <don@happycollision.com>
2021-03-01 21:32:21 -0500

    Fix media page layout

--- src/routes/media.svelte
+++ src/routes/media.svelte
@@ -2,11 +2,24 @@
   import Video from "$components/Video.svelte"
 </script>
 
-<Video title="Don's Reel" youtube="EouuLrgoMh4" width="{560}" height="{315}" />
+<div class="grid grid-cols-1 gap-4 max-w-xl m-auto md:grid-cols-2 md:max-w-4xl">
+  <div>
+    Don's Reel
+    <Video
+      title="Don's Reel"
+      youtube="EouuLrgoMh4"
+      width="{560}"
+      height="{315}"
+    />
+  </div>
 
-<Video
-  title="The Streets of Dublin (full song)"
-  youtube="mWU4wxTL-7k"
-  width="{560}"
-  height="{315}"
-/>
+  <div>
+    On the Streets of Dublin (full song)
+    <Video
+      title="The Streets of Dublin (full song)"
+      youtube="mWU4wxTL-7k"
+      width="{560}"
+      height="{315}"
+    />
+  </div>
+</div>
