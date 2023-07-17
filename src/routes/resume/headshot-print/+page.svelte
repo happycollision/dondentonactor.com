fe6770f0c14407c15541261c8e71ea95abeadec0
Don Denton <don@happycollision.com>
2023-07-16 22:19:37 -0400

    Drop name from headshot
    It just doesn't render correctly in PDF capture. No time to fix it.

--- src/routes/resume/headshot-print/+page.svelte
+++ src/routes/resume/headshot-print/+page.svelte
@@ -1,10 +1,1 @@
 <img src="/img/headshots/current.jpg" alt="Don Denton's headshot" />
-<h1 class="text-white absolute bottom-10 left-10 text-7xl drop-shadow-lg">Don<br />Denton</h1>
-
-<style>
-	@media print {
-		h1 {
-			color: white !important;
-		}
-	}
-</style>
